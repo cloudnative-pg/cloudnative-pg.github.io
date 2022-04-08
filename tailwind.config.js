c66a106b6775972288532c5c25c37a9f1d5c57a2
Simon Metson <simon.metson@enterprisedb.com>
2022-04-08 13:44:04 +0100

    simplify tailwind usage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,14 @@
-const { screens } = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+// https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+
 module.exports = {
-    content: [
-        "./layouts/**/*.html",
-        "./content/**/*.md",
-        "./content/**/*.html",
-        "./assets/scripts/**/*.js"
-    ],
-    variants: {},
     plugins: [],
-    // colors: colors,
-    corePlugins: {
-        textOpacity: false,
-        float: false,
-        clear: false,
-        divideColor: false,
-        divideOpacity: false,
-        divideStyle: false,
-        divideWidth: false,
-        fontVariantNumeric: false,
-        placeholderColor: false,
-        placeholderOpacity: false,
-        ringColor: false,
-        ringOffsetColor: false,
-        ringOffsetWidth: false,
-        ringOpacity: false,
-        ringWidth: false
-    }
+    theme: {
+        extend: {}
+    },
+    content: [
+        './layouts/**/*.{html,js}',
+        './assets/**/*.{html,js}',
+    ]
 }
