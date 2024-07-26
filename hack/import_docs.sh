c16d4e17f75bfa8d1abc796df01ac15df7851716
Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
2024-07-26 14:27:47 +0200

    docs: detect RC versions as preview (#205)
    Add the '/preview/' directory for the latest RC version.
    
    Add the version of CNPG on the left sidebar.
    
    Manage the robots.txt file to exclude the RC versions.
    
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- hack/import_docs.sh
+++ hack/import_docs.sh
@@ -6,6 +6,8 @@ set -o errexit -o nounset -o pipefail
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 cd "${REPO_ROOT}"
 
+CNPG_REPOSITORY=https://github.com/cloudnative-pg/cloudnative-pg.git
+
 # Require a version to be specified
 if [ "$#" -ne 1 ]; then
     echo "Usage: hack/import_docs.sh release-version"
@@ -18,6 +20,7 @@ fi
 release_version=${1#v}
 DOCDIR=$REPO_ROOT/assets/documentation
 TARGETDIR=$DOCDIR/$release_version
+PREVIEW_RELEASE=0
 
 # Create a temporary folder in which to clone the CNPG branch
 WORKDIR=$(mktemp -d)
@@ -27,14 +30,18 @@ BRANCH_NAME=release-$release_version
 # Use the main branch if we are not targeting a stable version
 if ! [[ "${release_version}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     BRANCH_NAME="main"
+    PREVIEW_RELEASE=1
 fi
 
 # Clone the branch
-git clone --depth 1 --branch $BRANCH_NAME git@github.com:cloudnative-pg/cloudnative-pg.git $WORKDIR/cnpg
+git clone --depth 1 --branch $BRANCH_NAME $CNPG_REPOSITORY $WORKDIR/cnpg
 
 mkdir -p $TARGETDIR
 
 pushd $WORKDIR/cnpg/docs
+# Set the version name in the `mkdocs.yml` file
+sed -i -s "s/^site_name: CloudNativePG\$/site_name: CloudNativePG v$release_version/" mkdocs.yml
+
 docker pull minidocks/mkdocs:latest
 docker run --rm -v "$(pwd):$(pwd)" -w "$(pwd)" \
     -v "$TARGETDIR:/var/cnpg" \
@@ -44,22 +51,41 @@ popd
 
 rm -rf $WORKDIR
 
-if [ ! -f content/docs/$release_version.md ]
+# Detect the current version (one with the highest release number that is not RC)
+latest_version=$(ls $DOCDIR | sort -n | grep -v '\-rc\?' | tail -1)
+LATEST_VERSION_LINK_DIR=current
+
+# Preview release
+if [ $PREVIEW_RELEASE -eq 1 ]
 then
-  hugo new docs/$release_version.md
-  git add content/docs/$release_version.md
+  ROBOTS_FILE="$REPO_ROOT/assets/robots.txt"
+  if [ ! -f "$ROBOTS_FILE" ]
+  then
+    cat > "$ROBOTS_FILE" <<EOF
+User-agent: *
+EOF
+  fi
+  cat >> "$ROBOTS_FILE" <<EOF
+Disallow: /documentation/$release_version/
+EOF
+  latest_version=$(ls $DOCDIR | sort -n | grep '\-rc\?' | tail -1)
+  LATEST_VERSION_LINK_DIR=preview
+else
+  # Standard release
+  if [ ! -f content/docs/$release_version.md ]
+  then
+    hugo new docs/$release_version.md
+    git add content/docs/$release_version.md
+  fi
 fi
 
-# Detect the current version (one with the highest release number)
-current_version=$(ls $DOCDIR | sort -n | tail -1)
-if [ -d $DOCDIR/current ]
-then
-    git rm -fr $DOCDIR/current
-fi
-
-# Copy the last available doc folder as 'current'
 # This is a hack as with GH Pages it is not possible
 # to issue server-side redirects
-cp -a $DOCDIR/$current_version $DOCDIR/current
-git add $DOCDIR/current
+if [ -d $DOCDIR/$LATEST_VERSION_LINK_DIR ]
+then
+  git rm -fr $DOCDIR/$LATEST_VERSION_LINK_DIR
+fi
+# Replace the content of the last available doc folder as 'current' or 'preview'
+cp -a $DOCDIR/$latest_version $DOCDIR/$LATEST_VERSION_LINK_DIR
+git add $DOCDIR/$LATEST_VERSION_LINK_DIR
 git add $TARGETDIR
